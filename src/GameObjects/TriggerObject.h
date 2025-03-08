ffb00682e0160b9bdc7aef3002a0db508da16f50
neogrammer <jlhardee1986@gmail.com>
2025-03-08 07:36:09 -0500

    cleaned up some files

--- src/GameObjects/TriggerObject.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "GameObject.h"
-
-class TriggerObject : public GameObject
-{
-public:
-	TriggerObject(std::string filename_, float x_ = 0.f, float y_ = 0.f);
-    ~TriggerObject();
-};
